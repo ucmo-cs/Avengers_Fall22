0bebb411ac5519952b87419d252922046f2fc834
Collin Vesel <cwv08710@ucmo.edu>
2022-10-16 21:44:29 -0500

    Changed the navbar to use the React Router. Hopefully this will make it easy to switch between pages.

--- commerce-bank/src/App.js
+++ commerce-bank/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Aptmnt_navbar from './pages/Aptmnt_navbar';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* Change title to whatever the title of the page will be. */}
-      <Aptmnt_navbar title="This Works!"/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Aptmnt_navbar/>}>
+            {/* Add the rest of the pages as Routes inside this one. */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- commerce-bank/src/pages/Aptmnt_navbar.js
+++ commerce-bank/src/pages/Aptmnt_navbar.js
@@ -1,41 +1,47 @@
 import React from "react";
+import {Outlet, Link} from "react-router-dom";
 import "./Aptmnt_navbar.css";
 
 function Aptmnt_navbar(props) {
     return (
         <>
-            <ul>
-                <li>
-                    {/* Image button */}
-                    <a id="img_btn">
-                        <img src="https://d1icd6shlvmxi6.cloudfront.net/gsc/8Q5MX4/53/2f/54/532f5456326a498990ea2620a7eed702/images/times/u32.jpg?pageId=6aa7ecff-1dd9-4d6c-8bdd-7868752c1c7a"/>
-                    </a>
-                </li>
-                <li>
-                    {/* Services */}
-                    <a>
-                        Services
-                    </a>
-                </li>
-                <li>
-                    {/* Locations */}
-                    <a>
-                        Locations
-                    </a>
-                </li>
-                <li>
-                    {/* Times */}
-                    <a>
-                        Times
-                    </a>
-                </li>
-                <li>
-                    {/* Details */}
-                    <a>
-                        More Details
-                    </a>
-                </li>
-            </ul>
+            {/* Use Links to make the buttons work. */}
+            <nav>
+                <ul>
+                    <li>
+                        {/* Image button */}
+                        <a id="img_btn">
+                            <img src="https://d1icd6shlvmxi6.cloudfront.net/gsc/8Q5MX4/53/2f/54/532f5456326a498990ea2620a7eed702/images/times/u32.jpg?pageId=6aa7ecff-1dd9-4d6c-8bdd-7868752c1c7a"/>
+                        </a>
+                    </li>
+                    <li>
+                        {/* Services */}
+                        <a>
+                            Services
+                        </a>
+                    </li>
+                    <li>
+                        {/* Locations */}
+                        <a>
+                            Locations
+                        </a>
+                    </li>
+                    <li>
+                        {/* Times */}
+                        <a>
+                            Times
+                        </a>
+                    </li>
+                    <li>
+                        {/* Details */}
+                        <a>
+                            More Details
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            
+            <Outlet/>
 
             {/* Hero image. Title can be customized. */}
             <h1 id="hero_img">{props.title}</h1>
