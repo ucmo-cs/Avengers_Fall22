4f7c5a7b003958684d74f2fbc9daa9fea0cef38f
James-Ferbrache <jsf07870@ucmo.edu>
2022-11-03 13:59:13 -0500

    Finished up the general styling for locations page

--- commerce-bank/src/pages/Locations.js
+++ commerce-bank/src/pages/Locations.js
@@ -7,22 +7,28 @@ class Locations extends React.Component {
     render() {
       return (
         <div>
-          <div id="rectangle">
-            <h1>Location 1</h1>
+          <div id="container">
+            <h1 id="item-a">Location 1</h1>
+            <h3 id="item-b">2 Appointsments Available</h3>
+            <p id="item-c">1.1  Miles away</p>
           <Link to="/times">
-            <button>Select</button>
+            <button type="button">Select</button>
           </Link>
           </div>
-          <div id="rectangle">
-            <h1>Location 2</h1>
+          <div id="container">
+            <h1 id="item-a">Location 2</h1>
+            <h3 id="item-b">1 Appointsments Available</h3>
+            <p id="item-c">5.2  Miles away</p>
             <Link to="/times">
-            <button>Select</button>
+            <button type="button">Select</button>
           </Link>
           </div>
-          <div id="rectangle">
-            <h1>Location 3</h1>
+          <div id="container">
+            <h1 id="item-a">Location 3</h1>
+            <h3 id="item-b">6 Appointsments Available</h3>
+            <p id="item-c">13.8  Miles away</p>
             <Link to="/times">
-            <button>Select</button>
+            <button type="button">Select</button>
           </Link>
           </div>
         </div>
