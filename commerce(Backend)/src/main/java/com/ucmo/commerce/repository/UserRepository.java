c7b73d331f57dd6c799ab98edd450a37222523fe
James-Ferbrache <73076154+James-Ferbrache@users.noreply.github.com>
2022-11-16 13:04:18 -0600

    Deleted GA starter project
    Will replace with the starter project given by TK

--- commerce(Backend)/src/main/java/com/ucmo/commerce/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ucmo.commerce.repository;
-
-import com.ucmo.commerce.domain.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User,Integer> {
-}
