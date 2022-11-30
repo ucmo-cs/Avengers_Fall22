beb4b9dd814a8ac1b7fffe9ed6f0e22c34dcea9c
PeytonTriplett1 <114117659+PeytonTriplett1@users.noreply.github.com>
2022-11-30 01:48:55 -0600

    Update Locations.js

--- Avengers/src/pages/aptmnt/Locations.js
+++ Avengers/src/pages/aptmnt/Locations.js
@@ -6,10 +6,22 @@ function Locations({updateLocation}){
       return (
         <div>
           <div id="container">
-            <h1 name="location1">Location 1</h1>
+            <h1 name="Lees Summit">Lees Summit</h1>
             <h3>2 Appointsments Available</h3>
             <p id="item-c">1.1  Miles away</p>
-            <button type="button" onClick={(e) => {updateLocation(e, document.getElementsByName('location1')[0].textContent)}}>Select</button>
+            <button type="button" onClick={(e) => {updateLocation(e, document.getElementsByName('Lees Summit')[0].textContent)}}>Select</button>
+          </div>
+        <div id="container">
+            <h1 name="Harrisonville">Harrisonville</h1>
+            <h3>7 Appointsments Available</h3>
+            <p id="item-c">7.4  Miles away</p>
+            <button type="button" onClick={(e) => {updateLocation(e, document.getElementsByName('Harrisonville')[0].textContent)}}>Select</button>
+          </div>
+        <div id="container">
+            <h1 name="Blue Springs">Blue Springs</h1>
+            <h3>5 Appointsments Available</h3>
+            <p id="item-c">13.1  Miles away</p>
+            <button type="button" onClick={(e) => {updateLocation(e, document.getElementsByName('Blue Springs')[0].textContent)}}>Select</button>
           </div>
         </div>
       );
